e25ce49b17afe261662a3936b198990b8359b9e3
Shiori Yoshida <g1920543@is.ocha.ac.jp>
2023-02-23 13:16:37 +0900

    Initial Commit

---
+++ Move app/Move_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Move_appApp.swift
+//  Move app
+//
+//  Created by 吉田史織 on 2023/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Move_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
